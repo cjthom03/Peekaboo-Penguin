9749a78b17dee7e1e4af79f0bcf2df462f439f6f
Daniel <d.chen323@gmail.com>
2018-06-24 14:20:37 -0700

    Add different view controllers and buttons to project

---
+++ Peekaboo/Peekaboo/directionsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  directionsViewController.swift
+//  Peekaboo
+//
+//  Created by Daniel Chen on 6/24/18.
+//  Copyright © 2018 Charles Thomas. All rights reserved.
+//
+
+import UIKit
+
+class directionsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
