930e0e3447a408e9daa08670805fb312d65f04c2
SecondCommand <nathaniel.cunha@gmail.com>
2018-06-26 09:49:24 -0700

    fix distance declaration

--- Peekaboo/Peekaboo/ViewController.swift
+++ Peekaboo/Peekaboo/ViewController.swift
@@ -12,7 +12,7 @@ import ARKit
 import Foundation
 
 class ViewController: UIViewController, ARSCNViewDelegate {
-    penguineToPOVDistance
+    var penguinToPOVDistance: Double = 0
     var penguinArray = [SCNNode]()
     var withinView = false
     
@@ -130,12 +130,16 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             let xDistance = currentPosition.x - penguinArray[0].position.x
             let yDistance = currentPosition.y - penguinArray[0].position.y
             let zDistance = currentPosition.z - penguinArray[0].position.z
-            penguineToPOVDistance = sqrt(xDistance * xDistance + yDistance * yDistance + zDistance * zDistance)
-            print(penguineToPOVDistance)
-            if (penguineToPOVDistance < 10 && !withinView) {
+            let tempPenguinToPOVDistance = sqrt(xDistance * xDistance + yDistance * yDistance + zDistance * zDistance)
+            print(tempPenguinToPOVDistance)
+            if (tempPenguinToPOVDistance <= 3 && !withinView) {
                 withinView = true
                 // play event
+            } else {
+                withinView = false
             }
+            penguinToPOVDistance = Double(tempPenguinToPOVDistance)
+            
         }
         
     }
